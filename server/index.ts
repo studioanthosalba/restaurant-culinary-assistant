4ef840128111664651c8f4c9c0a2af1671442f93
studioanthosalba <amaga@hotmail.it>
2025-04-03 16:49:45 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   res.on("finish", () => {
     const duration = Date.now() - start;
     if (path.startsWith("/api")) {
-      let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
+      let logLine = ${req.method} ${path} ${res.statusCode} in ${duration}ms;
       if (capturedJsonResponse) {
         logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
       }
@@ -60,11 +60,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
+  server.listen(port, () => {
+    log(serving on port ${port});
   });
 })();
